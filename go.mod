6921527e24c2bf5caa8a8aa445e806b798709bea
panlf <liangfeng_pan@163.com>
2020-12-11 23:49:26 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module transfer-information
+
+go 1.15
+
+require (
+	github.com/atotto/clipboard v0.1.2
+	github.com/go-vgo/robotgo v0.92.1
+	github.com/robotn/gohook v0.30.4
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/atotto/clipboard"
+	"github.com/go-vgo/robotgo"
+	_ "github.com/robotn/gohook"
+	hook "github.com/robotn/gohook"
+)
+
+func main() {
+
+	/*clipboard.WriteAll("你好啊")
+	text, _ := clipboard.ReadAll()
+	fmt.Println(text)*/
+
+	/*
+	var k = robotgo.AddEvents("ctrl","c")
+	if k  {
+		fmt.Println("you press... ", "ctrl+c")
+	}*/
+
+	robotgo.EventHook(hook.KeyDown, []string{"ctrl", "c"}, func(e hook.Event) {
+		fmt.Println("ctrl-c")
+
+		content,err := clipboard.ReadAll()
+
+		if err == nil {
+
+			fmt.Println(content)
+		}
+
+		robotgo.EventEnd()
+	})
+
+	s := robotgo.EventStart()
+	<-robotgo.EventProcess(s)
+}
